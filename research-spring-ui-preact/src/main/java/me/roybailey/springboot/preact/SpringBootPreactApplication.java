5125de781a6b38b38f893b413535df5d6fb1496b
roybailey <roybaileybiz@gmail.com>
2017-08-26 21:44:01 +0100

    preact sample bundle

---
+++ research-spring-ui-preact/src/main/java/me/roybailey/springboot/preact/SpringBootPreactApplication.java
@@ -0,0 +1,13 @@
+package me.roybailey.springboot.preact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPreactApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SpringBootPreactApplication.class, args);
+	}
+
+}
